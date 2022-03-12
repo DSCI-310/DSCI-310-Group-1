7734a8ff8a500417257f03d7cbfc67d381395206
Timothy Zhou <timothy_zhou@hotmail.com>
2022-03-12 12:57:51 -0800

    Updated Listfun and created Listfun test

--- src/listfun.py
+++ src/listfun.py
@@ -1,12 +1,25 @@
-import numpy as np
-import pandas as pd
-import warnings as wa
+# This is the abstract function for list, used in Results part of the notebook
+# @param pre is the preprocessor we're working w ith
+# @param transformers is the name of the transformers for pre
+# @param steps is the steps name for pre
+# @param features is the category of features we're focusing on
 
 def list_abs(pre, transformers, steps, features):
-    ret = list(
-    pre.named_transformers_[transformers]
-    .named_steps[steps]
-    .get_feature_names_out(features)
-    )
+    
+    #See if transformersna and steps are Strings
+    if (not isinstance(transformers, str)) or (not isinstance(steps, str)):
+        raise TypeError("Transformers and Steps need to be strings")
+        
+    #See if features is a list of Strings
+    if (not isinstance(features, list)):
+        raise TypeError("features need to be a list")
+        
+    #If both tests are passed: we generate the outcome and return
+    else:
+        ret = list(
+        pre.named_transformers_[transformers]
+        .named_steps[steps]
+        .get_feature_names_out(features)
+        )
     
     return ret
