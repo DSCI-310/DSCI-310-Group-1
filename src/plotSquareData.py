61dbfb61a8ce94f3355dab8490ed72524fffe83d
Andres Zepeda Perez <AndresZepeda137@gmail.com>
2022-03-12 17:41:01 -0600

    Fixed from tests and added plt.show

--- src/plotSquareData.py
+++ src/plotSquareData.py
@@ -9,56 +9,65 @@ def plot_square_data(x_df, y_df, desiredFeatures, titles, txt):
     # alongside the data in y_df and the respective titles 
     #
     # @param x_df dataframe containing dependent variables
-	# @param y_df dataframe containing independent variables
+    # @param y_df dataframe containing independent variables
     # @param desiredFeatures Target feature(s) in x_df you desire for your analysis in list form
     # @param titles The respective titles(s) desired for your plot(s) in list form
-	# @param txt the text to display as a string over the plot
+    # @param txt the text to display as a string over the plot
     #
     # @return axy for testing purposes on the features of the plot
     #
     # @examples
     # plot_square_data(X_train, y_train, ["feature1", "feature2", "feature3"], ["title1", "title2", "title3], "This is Plot 1")
     # plot_square_data(dependent, independednts, ["income"], ["retirement_age"], "Income and Retirement Age Relation")
-    X_df = None
-    y_df = None
     
-    if isinstance(x_df, pd.DataFrame) and isinstance(y_df, pd.DataFrame) and x_df.shape[0] == y_df.shape[0]:
-        if isinstance(desiredFeatures, list) and isinstance(desiredFeatures[0], str):
-            if isinstance(titles, list) and isinstance(titles[0], str) and len(titles) == len(desiredFeatures):
-				if ! isinstance(txt, str):
-					raise TypeError("The last argument is not a string")
+    #print("X Table")
+    #print(x_df)
+    #print(isinstance(x_df, pd.DataFrame))
+    #print(x_df.shape[0])
+    #print("Y Table")
+    #print(y_df)
+    #print(isinstance(y_df, pd.Series))
+    #print(y_df.shape[0])
 
-				# This will form a minimum sized box for the plots to go in, preferring horizontal boxes
-				# will aways preffer to create a box rather than make vertical spanning blocks if there are many
-				side_length_x = int(len(titles)**(1/2))
-				side_length_y = int(len(titles)**(1/2))
-				if side_length_x * side_length_y < len(titles):
-					side_length_x += 1
-					if side_length_x * side_length_y < len(titles):
-						side_length_y += 1
+    if isinstance(x_df, pd.DataFrame) and isinstance(y_df, pd.Series) and x_df.shape[0] == y_df.shape[0]:
+        if isinstance(desiredFeatures, list) and len(desiredFeatures) > 0 and isinstance(desiredFeatures[0], str):
+            if isinstance(titles, list) and len(titles) > 0 and isinstance(titles[0], str) and len(titles) == len(desiredFeatures):
+                if not isinstance(txt, str):
+                    raise TypeError("The last argument is not a string")
 
-
-				fig, axs = plt.subplots(side_length_y, side_length_x, figsize=(10,10))
-
-				x = 0
-				y = 0
-				try:
-					for i, feature in enumerate(desuredFeatures):
-						axs[y, x].scatter(x_df[desiredFeatures[i]], y_df)
-						axs[y, x].set_title(titles[i])
-						x += 1
-						if x > side_length_x:
-							x = 0
-							y += 1
-				except:
-					raise TypeError("desiredFeature is not in dependent dataframe")
+                # This will form a minimum sized box for the plots to go in, preferring horizontal boxes
+                # will aways preffer to create a box rather than make vertical spanning blocks if there are many
+                side_length_x = int(len(titles)**(1/2))
+                side_length_y = int(len(titles)**(1/2))
+                if side_length_x * side_length_y < len(titles):
+                    side_length_x += 1
+                    if side_length_x * side_length_y < len(titles):
+                        side_length_y += 1
 
 
-				plt.figtext(0.5, 0.05, txt, wrap=True, horizontalalignment='center', fontsize=12)
+                fig, axs = plt.subplots(side_length_y, side_length_x, figsize=(10,10))
 
-				return axs
-			else:
-				raise TypeError("titles is not a list of strings of length equal to desiredFeatures")
+                x = 0
+                y = 0
+                try:
+                    for i, feature in enumerate(desiredFeatures):
+                        #print(desiredFeatures[i])
+                        axs[y, x].scatter(x_df[desiredFeatures[i]], y_df)
+                        axs[y, x].set_title(titles[i])
+                        x += 1
+                        if x >= side_length_x:
+                            x = 0
+                            y += 1
+                except:
+                    raise TypeError("desiredFeature is not in dependent dataframe")
+
+
+                plt.figtext(0.5, 0.05, txt, wrap=True, horizontalalignment='center', fontsize=12)
+
+                plt.show()
+                return axs
+            else:
+                raise TypeError("titles is not a list of strings of length equal to desiredFeatures")
         else:
             raise TypeError("desiredFeatures is not a list of strings length at least 1")
     else:
