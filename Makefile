61f1fea3313406bc3f1f71352bb9c2a6178d98ba
danielhou13 <danielhou13@gmail.com>
2022-03-19 15:00:44 -0700

    Started making new scripts for other parts of analysis

--- Makefile
+++ Makefile
@@ -2,7 +2,11 @@
 # date: 2022-03-18
 
 #get the data from the web
-data/raw/student-mat.csv data/raw/student-por.csv data/raw/student.txt: src/gatherdata.py 
-    python src/gatherdata.py 'https://archive.ics.uci.edu/ml/machine-learning-databases/00320/student.zip' 'data/raw/student-mat.csv'
+data/raw/student-mat.csv: src/gatherdata.py
+	python src/gatherdata.py "archive.ics.uci.edu/ml/machine-learning-databases/00320/student.zip" "data/raw/student-mat.csv"
+    
+data/raw/student-mat-train.csv data/raw/student-mat-test.csv : src/splitter.py
+	python src/splitter.py "data/raw/student-mat.csv" 0.2 "data"
 
-#split the data into the train and test splits
+clean :
+	rm -f data/raw/student-mat.csv
---
+++ src/splitter.py
@@ -0,0 +1,33 @@
+"""splits data into training and test splits based on splitsize
+
+Usage: src/splitter.py <input_file> <size> <output_loc>
+
+Options:
+<input_file>    Path (including filename) to data file
+<size>    Proportion of testing split size
+<output_loc>    Path of where to locally write the file
+"""
+
+from docopt import docopt
+import splitxy
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+
+opt = docopt(__doc__)
+DELIMITERS = ". , ; : ? $ @ ^ < > # % ` ! * - = ( ) [ ] { } / \" '".split()
+
+def splitter(file, size, location):
+    df = pd.read_csv("data/raw/student-mat.csv",sep = ";")
+    train_df, test_df = train_test_split(df, test_size = size, random_state=100)
+    train_df.to_csv(os.path.join(location, "student-mat-train.csv"))
+    test_df.to_csv(os.path.join(location, "student-mat-test.csv"))
+    
+
+def main(file, size, location):
+    print("running")
+    splitter(file, float(size), location)
+    print("done")
+
+if __name__ == '__main__':
+    main(opt["<input_file>"], opt["<size>"], opt["<output_loc>"])
